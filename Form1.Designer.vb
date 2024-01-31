22fe1816bfc4ff590f03ea3a652b10a852220b8b
Chase Lopes <chase.lopes@kempston.co.za>
2024-01-31 13:27:53 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.gridPlaymat = New System.Windows.Forms.DataGridView()
+        Me.rbPlayer1 = New System.Windows.Forms.RadioButton()
+        Me.rbPlayer2 = New System.Windows.Forms.RadioButton()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.gridPlaymat, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'gridPlaymat
+        '
+        Me.gridPlaymat.AllowUserToAddRows = False
+        Me.gridPlaymat.AllowUserToDeleteRows = False
+        Me.gridPlaymat.AllowUserToResizeColumns = False
+        Me.gridPlaymat.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.gridPlaymat.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3})
+        Me.gridPlaymat.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
+        Me.gridPlaymat.EnableHeadersVisualStyles = False
+        Me.gridPlaymat.Location = New System.Drawing.Point(89, 133)
+        Me.gridPlaymat.Name = "gridPlaymat"
+        Me.gridPlaymat.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect
+        Me.gridPlaymat.Size = New System.Drawing.Size(343, 141)
+        Me.gridPlaymat.TabIndex = 0
+        '
+        'rbPlayer1
+        '
+        Me.rbPlayer1.AutoSize = True
+        Me.rbPlayer1.Checked = True
+        Me.rbPlayer1.Enabled = False
+        Me.rbPlayer1.Location = New System.Drawing.Point(89, 53)
+        Me.rbPlayer1.Name = "rbPlayer1"
+        Me.rbPlayer1.Size = New System.Drawing.Size(79, 17)
+        Me.rbPlayer1.TabIndex = 1
+        Me.rbPlayer1.TabStop = True
+        Me.rbPlayer1.Text = "Player 1 (X)"
+        Me.rbPlayer1.UseVisualStyleBackColor = True
+        '
+        'rbPlayer2
+        '
+        Me.rbPlayer2.AutoSize = True
+        Me.rbPlayer2.Enabled = False
+        Me.rbPlayer2.Location = New System.Drawing.Point(342, 53)
+        Me.rbPlayer2.Name = "rbPlayer2"
+        Me.rbPlayer2.Size = New System.Drawing.Size(80, 17)
+        Me.rbPlayer2.TabIndex = 2
+        Me.rbPlayer2.Text = "Player 2 (O)"
+        Me.rbPlayer2.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(217, 91)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 3
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = ""
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = ""
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = ""
+        Me.Column3.Name = "Column3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(511, 345)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.rbPlayer2)
+        Me.Controls.Add(Me.rbPlayer1)
+        Me.Controls.Add(Me.gridPlaymat)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.gridPlaymat, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents gridPlaymat As DataGridView
+    Friend WithEvents rbPlayer1 As RadioButton
+    Friend WithEvents rbPlayer2 As RadioButton
+    Friend WithEvents btnReset As Button
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+End Class
---
+++ Form1.vb
@@ -0,0 +1,209 @@
+﻿Public Class Form1
+
+    Dim intTurnCount As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        gridPlaymat.Rows.Add("", "", "")
+        gridPlaymat.Rows.Add("", "", "")
+        gridPlaymat.Rows.Add("", "", "")
+
+    End Sub
+
+    Private Sub gridPlaymat_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles gridPlaymat.CellClick
+        If rbPlayer1.Checked = True Then
+
+            If gridPlaymat.CurrentCell.Value = "O" Then
+                Exit Sub
+            End If
+
+            gridPlaymat.CurrentCell.Value = "X"
+            rbPlayer1.Checked = False
+            rbPlayer2.Checked = True
+
+        Else
+            If gridPlaymat.CurrentCell.Value = "X" Then
+                Exit Sub
+            End If
+
+            gridPlaymat.CurrentCell.Value = "O"
+            rbPlayer2.Checked = False
+            rbPlayer1.Checked = True
+        End If
+
+        intTurnCount = intTurnCount + 1
+
+        'MsgBox(gridPlaymat.CurrentCell.RowIndex.ToString() + "/" + gridPlaymat.CurrentCell.ColumnIndex.ToString())
+
+        '5
+
+        If intTurnCount >= 5 Then
+            CheckPlaymat()
+        End If
+
+
+
+
+    End Sub
+
+    Public Function CheckPlaymat()
+
+        Dim arrOptions(1) As String
+        arrOptions(0) = "X"
+        arrOptions(1) = "O"
+        Dim arrColor(5) As Integer
+
+        For w = 0 To 1
+
+            '-
+            For x = 0 To 2
+
+
+                If (gridPlaymat(0, x).Value = arrOptions(w) And gridPlaymat(1, x).Value = arrOptions(w) And gridPlaymat(2, x).Value = arrOptions(w)) Then
+                    arrColor(0) = 0
+                    arrColor(1) = x
+                    arrColor(2) = 1
+                    arrColor(3) = x
+                    arrColor(4) = 2
+                    arrColor(5) = x
+
+                    ResetMar(arrColor, arrOptions(w))
+                    Exit Function
+                End If
+
+            Next
+
+
+            '|
+            For x = 0 To 2
+
+                If (gridPlaymat(x, 0).Value = arrOptions(w) And gridPlaymat(x, 1).Value = arrOptions(w) And gridPlaymat(x, 2).Value = arrOptions(w)) Then
+                    arrColor(0) = x
+                    arrColor(1) = 0
+                    arrColor(2) = x
+                    arrColor(3) = 1
+                    arrColor(4) = x
+                    arrColor(5) = 2
+
+                    ResetMar(arrColor, arrOptions(w))
+                    Exit Function
+                End If
+
+
+            Next
+
+
+            '\
+            If (gridPlaymat(0, 0).Value = arrOptions(w) And gridPlaymat(1, 1).Value = arrOptions(w) And gridPlaymat(2, 2).Value = arrOptions(w)) Then
+                arrColor(0) = 0
+                arrColor(1) = 0
+                arrColor(2) = 1
+                arrColor(3) = 1
+                arrColor(4) = 2
+                arrColor(5) = 2
+
+                ResetMar(arrColor, arrOptions(w))
+                Exit Function
+            End If
+
+
+            '/
+            If (gridPlaymat(0, 2).Value = arrOptions(w) And gridPlaymat(1, 1).Value = arrOptions(w) And gridPlaymat(2, 0).Value = arrOptions(w)) Then
+                arrColor(0) = 0
+                arrColor(1) = 2
+                arrColor(2) = 1
+                arrColor(3) = 1
+                arrColor(4) = 2
+                arrColor(5) = 0
+
+                ResetMar(arrColor, arrOptions(w))
+                Exit Function
+            End If
+
+        Next
+
+
+        Dim RowCount = 0
+
+        For x = 0 To 2
+
+            For z = 0 To 2
+
+                If Not gridPlaymat(x, z).Value = "" Then
+
+                    RowCount = RowCount + 1
+
+                End If
+
+
+            Next
+
+        Next
+
+        If RowCount = 9 Then
+
+            For x = 0 To 2
+
+                For z = 0 To 2
+
+                    If Not gridPlaymat(x, z).Value = "" Then
+
+                        gridPlaymat(x, z).Style.BackColor = Color.Red
+
+                    End If
+
+
+                Next
+
+            Next
+
+            ResetMar(arrColor, "L")
+            Exit Function
+
+        End If
+
+
+
+    End Function
+
+    Public Function ResetMar(strType() As Integer, strWinner As String)
+
+        If Not strWinner = "L" Then
+            gridPlaymat(strType(0), strType(1)).Style.BackColor = Color.LightBlue
+            gridPlaymat(strType(2), strType(3)).Style.BackColor = Color.LightBlue
+            gridPlaymat(strType(4), strType(5)).Style.BackColor = Color.LightBlue
+            MsgBox(strWinner + " wins!")
+        Else
+            MsgBox("No winner!")
+        End If
+
+
+
+
+        gridPlaymat.Rows.Clear()
+        rbPlayer1.Checked = True
+        rbPlayer2.Checked = False
+        gridPlaymat.Rows.Add("", "", "")
+        gridPlaymat.Rows.Add("", "", "")
+        gridPlaymat.Rows.Add("", "", "")
+        intTurnCount = 0
+
+    End Function
+
+
+    Private Sub gridPlaymat_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles gridPlaymat.CellContentClick
+
+
+
+
+    End Sub
+
+    Private Sub rbPlayer1_CheckedChanged(sender As Object, e As EventArgs) Handles rbPlayer1.CheckedChanged
+
+
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        Dim arrColor(4) As Integer
+        ResetMar(arrColor, "L")
+    End Sub
+End Class
